1b907b0525452d75484c82dec5a68a9200c3dbb4
AmrDalati <Egram.963@gmail.com>
2024-01-24 17:39:19 +0200

    CRUD User

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,6 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'role' => ($this->role == 1 ? 'admin' : 'dataentry'),
-            'token' => $this->createToken($request->userAgent())->plainTextToken,
         ];
     }
 }
