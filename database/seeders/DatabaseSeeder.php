111e963df3e3b47f5e5a94b97b0fc459c71a0727
daniel-albaroudi <danielalbaroudi@gmail.com>
2024-01-29 07:57:22 -0800

    fdgf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             CoachSeeder::class,
             SpecializetionSeeder::class,
             AmountSeeder::class,
+            EmployeeSeeder::class,
+            BrunchSeeder::class,
         ]);
     }
 }
